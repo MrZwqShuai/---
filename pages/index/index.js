e4f59054269a9f7246518feef5df8f4b7c575483
wxp <310082609@qq.com>
2018-03-01 15:43:15 +0800

    zwq

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,10 +7,41 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // 首页导航
+    navs: [{
+      url: '/images/nav01.png'
+    }, {
+      url: '/images/nav02.png'
+    }, {
+      url: '/images/nav03.png'
+    }, {
+      url: '/images/nav04.png'
+    }, {
+      url: '/images/nav05.png'
+    }, {
+      url: '/images/nav06.png'
+    }, {
+      url: '/images/nav07.png'
+    }, {
+      url: '/images/nav08.png'
+    }],
+    rewards:[{
+      img: '/images/nav01.png',
+      explain: '立即兑换'
+    }, {
+      img: '/images/nav01.png',
+      explain: '立即兑换'
+      }, {
+        img: '/images/nav01.png',
+        explain: '立即兑换'
+    }, {
+      img: '/images/nav01.png',
+      explain: '立即兑换'
+    },]
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +52,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,12 +74,35 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onReady: function () {
+    this.getUserInfo();
+  },
+  getUserInfo: function (e) {
+    // console.log(e)
+    // app.globalData.userInfo = e.detail.userInfo
+    // this.setData({
+    //   userInfo: e.detail.userInfo,
+    //   hasUserInfo: true
+    // })
+    // 获取用户在贯日积分中的个人信息
+    this.getPoint();
+  },
+  // 获取用户在贯日积分活动中的信息
+  getPoint: function () {
+    console.log(this.data.userInfo);
+    this.data.userInfo.rank = '';
+    this.data.userInfo.fragment = '';
+    this.data.userInfo.fragment = '';
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      'userInfo.rank': '100',
+      'userInfo.fragment': '100',
+      'userInfo.points': '10'
+    });
+  },
+  /**
+   * 兑换礼品
+   */
+  exchange: function() {
+    console.log('兑换礼品');
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -8,27 +8,71 @@ Page({
   data: {
     loginValidate: {},
     countdown: '我要获取',
-    validMessage: '请输入正确的手机号'
+    validMessage: '请输入正确的手机号',
+    times: 59,
+    isCountDown: false,
+    // 发送验证码禁用点击
+    isNeedDsiable: false
   },
-
+  // 显示发送验证码控件
+  showSendCodeBtn() {
+    this.setData({
+      isNeedDsiable: false
+    });
+  },
+  // 隐藏发送验证码控件
+  hideSendCodeBtn() {
+    this.setData({
+      isNeedDsiable: true
+    });
+  },
+  sendCode: function () {
+    console.log(999);
+    this.setData({
+      isCountDown: true
+    });
+    this.countDown();
+  },
+  countDown: function () {
+    this.hideSendCodeBtn();
+    let timer = setInterval(() => {
+      this.data.times--;
+      this.setData({
+        times: this.data.times
+      });
+      console.log(this.data.times);
+      if (this.data.times <= 0) {
+        clearInterval(timer);
+        this.setData({
+          isCountDown: false
+        });
+        this.data.times = 59;
+        this.showSendCodeBtn();
+      }
+    }, 100);
+  },
   /**
    * 登录
    */
   login: function (phoneNum) {
-    if(!this.loginValidate.isValid(phoneNum)){
+    if (!this.loginValidate.isValid(phoneNum)) {
       wx.showLoading({
         title: '请输入正确的手机号',
         icon: 'none',
         duration: 1000
       })
     }
+    // 登录跳转
+    wx.navigateTo({
+      url: '../index/index',
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -42,41 +86,41 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
