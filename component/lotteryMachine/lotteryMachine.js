e020abe7e1408098875fd6fc6fb8144521333423
wxp <310082609@qq.com>
2018-03-13 13:07:32 +0800

    zwq

--- component/lotteryMachine/lotteryMachine.js
+++ component/lotteryMachine/lotteryMachine.js
@@ -11,13 +11,77 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    lottery: [{
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }, {
+      rewardName: '小米MAX',
+      rewardImg: ''
+    }],
+    selectedIdx: null,
+    lotteryStruct: [0, 1, 2, 5, 8, 7, 6, 3, 0]
   },
-
+  ready() {
+    // this._lotterymainFn();
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
+    _lotterymainFn() {
+      let timer = this._lotteryTimer(100)
+      let clearTimer = setTimeout(() => {
+        this._clearTimer(timer);
+        clearTimeout(clearTimer);
+        console.log(this.data.selectedIdx);
+        this._lotteryTimer(500)
+      }, this._randomTime());
+    },
+    _lotteryTimer(speed, lotteryIdx) {
+      let i = 0;
+      let timer = setInterval(() => {
+        this.setData({
+          selectedIdx: this.data.lotteryStruct[i]
+        });
+        i++
+        if (i >= 9) {
+          i = 1;
+        }
+      }, speed);
+      return timer;
+    },
+    _play(event) {
+      let playIdx = event.currentTarget.dataset.playidx;
+      if (playIdx === 4) {
+        this._lotterymainFn();
+      }
+    },
+    _clearTimer(timer) {
+      clearInterval(timer);
+    },
+    _randomTime() {
+      return Math.floor(Math.random() * 1000) + 2000
+    }
+  },
 })
