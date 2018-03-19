49b137c5068ac814936d490610f0290ad342e380
liwl <lwl.286131719@163.com>
2018-03-19 14:18:19 +0800

    路径更改为https

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
-const BASEURL = 'http://www.youmahui.net:8083';
-const IMGURL = 'http://www.youmahui.net:8083/ezShop';
+const BASEURL = 'https://www.youmahui.net:8083';
+const IMGURL = 'https://www.youmahui.net:8083/ezShop';
 module.exports = {
   url: BASEURL,
   imgUrl: IMGURL,
